c073580ab0e1b3cc9e892af9b589c9c6683784b4
green-creeper <andrey.korlyuk@gmail.com>
2013-01-30 15:46:01 +0200

    added template from http://www.html5webtemplates.co.uk

--- target/scala-2.9.1/src_managed/main/views/html/index.template.scala
+++ target/scala-2.9.1/src_managed/main/views/html/index.template.scala
@@ -22,9 +22,91 @@ object index extends BaseScalaTemplate[play.api.templates.Html,Format[play.api.t
 Seq[Any](format.raw/*1.19*/("""
 
 """),_display_(Seq[Any](/*3.2*/main("Welcome to Play 2.0")/*3.29*/ {_display_(Seq[Any](format.raw/*3.31*/("""
-    
-    """),_display_(Seq[Any](/*5.6*/play20/*5.12*/.welcome(message))),format.raw/*5.29*/("""
-    
+<div id="site_content">
+	<div id="sidebar_container">
+		<img class="paperclip" src="style/paperclip.png" alt="paperclip" />
+		<div class="sidebar">
+			<!-- insert your sidebar items here -->
+			<h3>Latest News</h3>
+			<h4>What's the News?</h4>
+			<h5>1st July 2011</h5>
+			<p>
+				Put your latest news item here, or anything else you would like in the sidebar!
+				<br />
+				<a href="#">Read more</a>
+			</p>
+		</div>
+		<img class="paperclip" src="style/paperclip.png" alt="paperclip" />
+		<div class="sidebar">
+			<h3>Newsletter</h3>
+			<p>
+				If you would like to receive our newletter, please enter your email address and click 'Subscribe'.
+			</p>
+			<form method="post" action="#" id="subscribe">
+				<p style="padding: 0 0 9px 0;">
+					<input class="search" type="text" name="email_address" value="your email address" onclick="javascript: document.forms['subscribe'].email_address.value=''" />
+				</p>
+				<p>
+					<input class="subscribe" name="subscribe" type="submit" value="Subscribe" />
+				</p>
+			</form>
+		</div>
+		<img class="paperclip" src="style/paperclip.png" alt="paperclip" />
+		<div class="sidebar">
+			<h3>Latest Blog</h3>
+			<h4>Website Goes Live</h4>
+			<h5>1st July 2011</h5>
+			<p>
+				We have just launched our new website. Take a look around, we'd love to know what you think.....
+				<br />
+				<a href="#">read more</a>
+			</p>
+		</div>
+	</div>
+	<div id="content">
+		<!-- insert the page content here -->
+		<h1>Welcome to the simplestyle_7 template</h1>
+		<p>
+			This standards compliant, simple, fixed width website template is released as an 'open source' design (under a
+			<a href="http://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 Licence</a>
+			), which means that you are free to download and use it for anything you want (including modifying and amending it). All I ask is that you leave the 'design from HTML5webtemplates.co.uk' link in the footer of the template, but other than that...
+		</p>
+		<p>
+			This template is written entirely in <strong>HTML5</strong>
+			and <strong>CSS</strong>
+			, and can be validated using the links in the footer.
+		</p>
+		<p>
+			You can view more free HTML5 web templates
+			<a href="http://www.html5webtemplates.co.uk">here</a>
+			.
+		</p>
+		<p>
+			This template is a fully functional 5 page website, with an
+			<a href="examples.html">examples</a>
+			page that gives examples of all the styles available with this design.
+		</p>
+		<h2>Browser Compatibility</h2>
+		<p>This template has been tested in the following browsers:</p>
+		<ul>
+			<li>Internet Explorer 8</li>
+			<li>Internet Explorer 7</li>
+			<li>FireFox 3.5</li>
+			<li>Google Chrome 6</li>
+			<li>Safari 4</li>
+		</ul>
+	</div>
+</div>
+<div id="footer">
+	<p>
+		Copyright &copy; simplestyle_7 |
+		<a href="http://validator.w3.org/check?uri=referer">HTML5</a>
+		|
+		<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
+		|
+		<a href="http://www.html5webtemplates.co.uk">design from HTML5webtemplates.co.uk</a>
+	</p>
+</div>
 """)))})))}
     }
     
@@ -37,11 +119,11 @@ Seq[Any](format.raw/*1.19*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Wed Jan 23 18:13:37 GMT+02:00 2013
+                    DATE: Wed Jan 30 15:44:47 GMT+02:00 2013
                     SOURCE: /Users/andrey/IdeaProjects/SimpleBlog/app/views/index.scala.html
-                    HASH: a9468e14516db88237e3431e8b54cf7780705202
-                    MATRIX: 505->1|599->18|636->21|671->48|710->50|755->61|769->67|807->84
-                    LINES: 19->1|22->1|24->3|24->3|24->3|26->5|26->5|26->5
+                    HASH: 46deb261b09dee18876f9d2f08689c1a47a63a35
+                    MATRIX: 505->1|599->18|636->21|671->48|710->50
+                    LINES: 19->1|22->1|24->3|24->3|24->3
                     -- GENERATED --
                 */
             
--- target/scala-2.9.1/src_managed/main/views/html/main.template.scala
+++ target/scala-2.9.1/src_managed/main/views/html/main.template.scala
@@ -20,21 +20,55 @@ object main extends BaseScalaTemplate[play.api.templates.Html,Format[play.api.te
         _display_ {
 
 Seq[Any](format.raw/*1.32*/("""
-
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <title>"""),_display_(Seq[Any](/*7.17*/title)),format.raw/*7.22*/("""</title>
-        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*8.54*/routes/*8.60*/.Assets.at("stylesheets/main.css"))),format.raw/*8.94*/("""">
-        <link rel="shortcut icon" type="image/png" href=""""),_display_(Seq[Any](/*9.59*/routes/*9.65*/.Assets.at("images/favicon.png"))),format.raw/*9.97*/("""">
-        <script src=""""),_display_(Seq[Any](/*10.23*/routes/*10.29*/.Assets.at("javascripts/jquery-1.7.1.min.js"))),format.raw/*10.74*/("""" type="text/javascript"></script>
-    </head>
-    <body>
-        """),_display_(Seq[Any](/*13.10*/content)),format.raw/*13.17*/("""
-    </body>
-</html>
-"""))}
+<head>
+	<title>"""),_display_(Seq[Any](/*6.10*/title)),format.raw/*6.15*/("""</title>
+	<meta name="description" content="website description" />
+	<meta name="keywords" content="website keywords, website keywords" />
+	<link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*9.47*/routes/*9.53*/.Assets.at("stylesheets/style.css"))),format.raw/*9.88*/("""">
+	<link rel="shortcut icon" type="image/png" href=""""),_display_(Seq[Any](/*10.52*/routes/*10.58*/.Assets.at("images/favicon.png"))),format.raw/*10.90*/("""">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<script src=""""),_display_(Seq[Any](/*12.16*/routes/*12.22*/.Assets.at("javascripts/jquery-1.7.1.min.js"))),format.raw/*12.67*/("""" type="text/javascript"></script>
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine&amp;v1" />
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz" />
+</head>
+<body>
+	<div id="main">
+		<div id="header">
+			<div id="logo">
+				<h1>
+					simplestyle
+					<a href="#">_7</a>
+				</h1>
+				<div class="slogan">Cool slogan goes here!</div>
+			</div>
+			<div id="menubar">
+				<ul id="menu">
+					<!-- put class="current" in the li tag for the selected page - to highlight which page you're on -->
+					<li class="current">
+						<a href="index.html">Home</a>
+					</li>
+					<li>
+						<a href="examples.html">Examples</a>
+					</li>
+					<li>
+						<a href="page.html">A Page</a>
+					</li>
+					<li>
+						<a href="another_page.html">Another Page</a>
+					</li>
+					<li>
+						<a href="contact.html">Contact Us</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		"""),_display_(Seq[Any](/*47.4*/content)),format.raw/*47.11*/("""
+	</div>
+</body>
+</html>"""))}
     }
     
     def render(title:String,content:Html) = apply(title)(content)
@@ -46,11 +80,11 @@ Seq[Any](format.raw/*1.32*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Wed Jan 23 18:13:37 GMT+02:00 2013
+                    DATE: Wed Jan 30 15:41:53 GMT+02:00 2013
                     SOURCE: /Users/andrey/IdeaProjects/SimpleBlog/app/views/main.scala.html
-                    HASH: 6a9d3e71a4db67b23442aa0e15837c7026dc07bb
-                    MATRIX: 509->1|616->31|704->84|730->89|827->151|841->157|896->191|992->252|1006->258|1059->290|1120->315|1135->321|1202->366|1305->433|1334->440
-                    LINES: 19->1|22->1|28->7|28->7|29->8|29->8|29->8|30->9|30->9|30->9|31->10|31->10|31->10|34->13|34->13
+                    HASH: 9eacb44764fa8043cb0696dcbb0f9e1b59665473
+                    MATRIX: 509->1|616->31|692->72|718->77|938->262|952->268|1008->303|1098->357|1113->363|1167->395|1292->484|1307->490|1374->535|2362->1488|2391->1495
+                    LINES: 19->1|22->1|27->6|27->6|30->9|30->9|30->9|31->10|31->10|31->10|33->12|33->12|33->12|68->47|68->47
                     -- GENERATED --
                 */
             
